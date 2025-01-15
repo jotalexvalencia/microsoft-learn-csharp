8783799b084af7f5b294b46f6bd50bc18380be6d
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-15 14:02:11 -0500

    use of TryParse example #3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization; 
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
-string value = "102";
+string value = "bad";
 int result = 0;
 if (int.TryParse(value, out result))
 {
@@ -10,4 +10,5 @@ else
 {
    Console.WriteLine("Unable to report the measurement.");
 }
-Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+if (result > 0)
+   Console.WriteLine($"Measurement (w/ offset): {50 + result}");
