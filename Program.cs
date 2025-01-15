1d74edd97cab10f0a832fe5358bbe48806446372
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-15 13:38:03 -0500

    use (int) and Convert

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization; 
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
-string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(result);
+int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value2);
