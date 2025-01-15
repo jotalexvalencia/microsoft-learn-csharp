ac3091fabaf0b713977d83d1bbce103b7ab68710
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-15 13:25:49 -0500

    Ejercicio de conversión de datos

--- Program.cs
+++ Program.cs
@@ -1,19 +1,6 @@
-﻿int[] data = new int[3];
-string shortenedString = "Hello World!";
-Console.WriteLine(shortenedString);
-int val_A = 2;
-int val_B = val_A;
-val_B = 5;
-
-Console.WriteLine("--Value Types--");
-Console.WriteLine($"val_A: {val_A}");
-Console.WriteLine($"val_B: {val_B}");
-
-int[] ref_A= new int[1];
-ref_A[0] = 2;
-int[] ref_B = ref_A;
-ref_B[0] = 5;
-
-Console.WriteLine("--Reference Types--");
-Console.WriteLine($"ref_A[0]: {ref_A[0]}");
-Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+﻿using System.Globalization; 
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
