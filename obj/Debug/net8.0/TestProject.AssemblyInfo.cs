061764a03b4e7c3edd0dd2e523e0da697de39215
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-16 17:13:03 -0500

    verificamos que el elemento en la posicion 0 no es nulo para mostrar el mensaje y convertir a minusculas

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77a421f89e19fe5256e90cab6d1844545591054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824bf7c609c7eda9762df5cef6612767bd0be917")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
