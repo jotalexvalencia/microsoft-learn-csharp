32c3404393f347a6cae51d14c89fb978b3d4b78b
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-16 16:35:46 -0500

    Metodo SORT en Array

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20be352acef67e2867c55007ee8ca8a070790182")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9f1bee0f610057aef48b414e3ce320b63a7100")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
