2ada47016423ae0bf93a099f2c33c9e5e6665b66
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-16 17:20:12 -0500

    metodo resize reduce el tamaño del array a 3

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061764a03b4e7c3edd0dd2e523e0da697de39215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9197e56d3cd1bfb65e187b675300168109c87ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
