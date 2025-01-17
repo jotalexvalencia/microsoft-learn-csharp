0ed5d9696eef8d7c4c026e1f60a33536d99f5a39
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-17 10:27:47 -0500

    inviertiendo cada palabra de un string

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
 ﻿using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = String.Join(",", valueArray);
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// 1. Dividir en palabras
+string[] words = pangram.Split(' ');
+
+
+// 2. Crear array para palabras invertidas
+string[] reversedWords = new string[words.Length];
+
+
+// 3. Invertir cada palabra
+foreach (string word in words)
+{
+  char[] letters = word.ToCharArray();
+  Array.Reverse(letters);
+  string reversedWord = new string(letters);
+  reversedWords[Array.IndexOf(words, word)] = reversedWord;
+}
+
+
+// 4. Unir palabras invertidas con espacios
+
+string result = string.Join(" ", reversedWords);
 Console.WriteLine(result);
 
-string[] items = result.Split(',');
-foreach (string item in items)
-{
-  Console.WriteLine(item);
-}
+
+
+
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ea68a71211cc91406be08af0e92ca73506ff82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612432477ad4dac92702cabf5382192f7f01e65a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
