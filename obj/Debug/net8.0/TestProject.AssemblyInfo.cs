b42c49bfc51340a1d27e0983f5a6dbd558418e9b
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-02-07 10:55:16 -0500

    Ejercicio: Prueba de paso por referencia

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e71fe2185b1e386551848df347cc079b7774d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e44224ccd358a2e5a69289942713a7deb0c0f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
