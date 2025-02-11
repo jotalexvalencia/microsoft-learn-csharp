7fbcac47f918975ead753f3a7855e0d60febe541
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-02-11 17:20:33 -0500

    7 Ejercicio: Completar el desafío para agregar métodos para que el juego sea jugable

--- Program.cs
+++ Program.cs
@@ -1,44 +1,51 @@
-﻿int target = 80;
-int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
-int[,] result = TwoCoins(coins, target);
+﻿Random random = new Random();
 
-if (result.Length == 0)
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
 {
-  Console.WriteLine("No two coins make change");
+  PlayGame();
 }
-else
+
+void PlayGame()
 {
-  Console.WriteLine("Change found at positions:");
-  for (int i = 0; i < result.GetLength(0); i++)
+  var play = true;
+
+  while (play)
   {
-    if (result[i, 0] == -1)
-    {
-      break;
-    }
-    Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+    var target = GetTarget();
+    var roll = GetRoll();
+
+    Console.WriteLine($"Roll a number greater than {target} to win!");
+    Console.WriteLine($"You rolled a {roll}");
+    Console.WriteLine(WinOrLose(roll, target));
+    Console.WriteLine("\nPlay again? (Y/N)");
+
+    play = ShouldPlay();
   }
 }
 
-int[,] TwoCoins(int[] coins, int target)
+bool ShouldPlay()
 {
-  int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
-  int count = 0;
+  string? input = Console.ReadLine();
+  bool result = input == "Y" ? true : false;
+  return result;
 
-  for (int curr = 0; curr < coins.Length; curr++)
-  {
-    for (int next = curr + 1; next < coins.Length; next++)
-    {
-      if (coins[curr] + coins[next] == target)
-      {
-        result[count, 0] = curr;
-        result[count, 1] = next;
-        count++;
-      }
-      if (count == result.GetLength(0))
-      {
-        return result;
-      }
-    }
-  }
-  return (count == 0) ? new int[0, 0] : result;
+}
+
+string WinOrLose(int roll, int target)
+{
+  return roll > target ? "You win!" : "You lose!";
+
+}
+
+int GetTarget()
+{
+  return random.Next(1, 6);
+
+}
+
+int GetRoll()
+{
+  return random.Next(1, 7);
 }
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338f506f36bbe92b9c68153c0b624078e9b7b8ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6263aaa7c57656995c25121968ccdae5990004")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
