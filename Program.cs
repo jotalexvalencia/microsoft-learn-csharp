055934e69c25cc19929c7dbe964bbd5a6b3998c9
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-02-11 14:32:10 -0500

    ejercicio devolver números de métodos

--- Program.cs
+++ Program.cs
@@ -1,29 +1,10 @@
-﻿double total = 0;
-double minimumSpend = 30.00;
+﻿double usd = 23.73;
+int vnd = UsdToVnd(usd);
 
-double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
-double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+Console.WriteLine($"${usd} USD = ${vnd} VND");
 
-for (int i = 0; i < items.Length; i++)
+int UsdToVnd(double usd)
 {
-  total += GetDiscountedPrice(i);
-}
-
-total -= TotalMeetsMinimum() ? 5.00 : 0.00;
-
-Console.WriteLine($"Total: ${FormatDecimal(total)}");
-
-double GetDiscountedPrice(int itemIndex)
-{
-  return items[itemIndex] * (1 - discounts[itemIndex]);
-}
-
-bool TotalMeetsMinimum()
-{
-  return total >= minimumSpend;
-}
-
-string FormatDecimal(double input)
-{
-  return input.ToString().Substring(0, 5);
+  int rate = 23500;
+  return (int)(rate * usd);
 }
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66fcdfcac020bcd1d2e55ce60456f9734a55324e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5b05a6be24d181bde1f587fee3f21f4d8da1b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
