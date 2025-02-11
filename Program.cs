6a6263aaa7c57656995c25121968ccdae5990004
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-02-11 16:25:07 -0500

    Ejercicio 2 devolver matrices de métodos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿int target = 60;
+﻿int target = 80;
 int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
-int[] result = TwoCoins(coins, target);
+int[,] result = TwoCoins(coins, target);
 
 if (result.Length == 0)
 {
@@ -8,22 +8,37 @@ if (result.Length == 0)
 }
 else
 {
-  Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+  Console.WriteLine("Change found at positions:");
+  for (int i = 0; i < result.GetLength(0); i++)
+  {
+    if (result[i, 0] == -1)
+    {
+      break;
+    }
+    Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+  }
 }
 
-int[] TwoCoins(int[] coins, int target)
+int[,] TwoCoins(int[] coins, int target)
 {
+  int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+  int count = 0;
+
   for (int curr = 0; curr < coins.Length; curr++)
   {
     for (int next = curr + 1; next < coins.Length; next++)
     {
       if (coins[curr] + coins[next] == target)
       {
-        return new int[] { curr, next };
+        result[count, 0] = curr;
+        result[count, 1] = next;
+        count++;
       }
-
+      if (count == result.GetLength(0))
+      {
+        return result;
+      }
     }
   }
-
-  return new int[0];
+  return (count == 0) ? new int[0, 0] : result;
 }
